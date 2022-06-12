f921ac6d61601daf2ab3683aff59cb6d9dc73853
rusterz <aaadev32@gmail.com>
2022-06-12 15:16:33 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import _ from 'lodash';
 import myName from './myName';
-
+import './style.css';
+import Icon from './dog.jpg';
 
 function component() {
   const element = document.createElement('div');
-
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.textContent = myName('rusterz');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+
   return element;
 }
 
